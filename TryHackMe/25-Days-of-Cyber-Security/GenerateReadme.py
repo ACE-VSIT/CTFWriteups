dbc9c51dc5198a9861dcf9fefd8d377d1fad3976
preetamherald <preetamherald@gmail.com>
2021-06-24 23:42:28 +0530

    bug fixes

--- TryHackMe/25-Days-of-Cyber-Security/GenerateReadme.py
+++ TryHackMe/25-Days-of-Cyber-Security/GenerateReadme.py
@@ -7,7 +7,7 @@ contents.remove("GenerateReadme.py")
 def generateLine(content,link):
     content = content[3:]
     data = content.split(' ',1)
-    line = "| Day " + data[0] + " | " + data[1][:-3] + " | [Click Here]('" + link + "')\n"
+    line = "| Day " + data[0] + " | " + data[1][:-3] + " | [Click Here](" + link.replace(" ", "%20") + ")\n"
     return (line)
 
 contentList = ["## This folder includes all writeups for 25 days of cybersecurity at tryhackme\n", "\n", "| Day | Title | Link |\n", "| ----| ----  | ---- |\n"]
